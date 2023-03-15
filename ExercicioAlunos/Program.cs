4bff0f7ab4ca047d598fdc8d487611e3da3939a7
Davi Santos <100373892+Guararai@users.noreply.github.com>
2023-03-15 15:40:18 -0300

    Add files via upload

---
+++ ExercicioAlunos/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = "Brian Souza";
+        aluno1.nota1 = 8.2;
+        aluno1.nota2 = 7.8;
+
+        aluno1.mensagem();  
+
+    }
+    
+}
---
+++ desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿   namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Alunos aluno1 = new Alunos();
+        
+  
+        aluno1.mensagem();  
+      
+        
+
+    }
+    
+}
+

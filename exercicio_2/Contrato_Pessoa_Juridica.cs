1e45957553409215e6a152f4d7c181031631dc72
Davi Santos <100373892+Guararai@users.noreply.github.com>
2023-05-24 12:00:38 -0300

    Add files via upload

---
+++ exercicio_2/Contrato_Pessoa_Juridica.cs
@@ -0,0 +1,20 @@
+﻿public class Contrato_Pessoa_Juridica:Contrato
+{
+
+    private string inscricaoEstadual;
+    private string cnpj;
+
+    public Contrato_Pessoa_Juridica(string detalhes, double valor, string cnpj, string inscricao_estadual)
+        : base(detalhes, valor)
+    {
+        this.cnpj = cnpj;
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public override void ExibirDetalhes()
+    {
+        base.ExibirDetalhes();
+        Console.WriteLine("CNPJ: " + cnpj);
+        Console.WriteLine("Inscrição Estadual: " + inscricaoEstadual);
+    }
+}
---
+++ exercicio_2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicio_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Contrato_Pessoa_Juridica contrato_Pessoa_Juridica = new Contrato_Pessoa_Juridica("contrato de pessoa jurídica :", 5000.0, "12.345.678/0001-00", "123456789");
+        contrato_Pessoa_Juridica.Exibir();
+
+        Contrato_Pessoa_Fisica contrato_Pessoa_Fisica = new Contrato_Pessoa_Fisica("contrato de pessoa física :", 1000.0, "123.456.789-00", 30);
+        contratoPessoaFisica.Exibir();
+        Console.WriteLine();
+
+
+    }
+        
+    
+}
+
